5f8678ec5361418e41a89492aad9ade2d79438ea
samaaali76 <semaaali76@gmail.com>
2026-04-13 19:44:12 +0200

    Update animation.cpp

--- MetroStationSimulator/MetroStationSimulator/animation.cpp
+++ MetroStationSimulator/MetroStationSimulator/animation.cpp
@@ -1,1 +1,18 @@
+#include <GL/glut.h>
 #include "animation.h"
+
+// بداية القطر بره الشاشة
+float trainPosition = -10.0f;
+
+// ================= Animation Loop =================
+void update(int value) {
+
+    // حركة القطر (يمين)
+    trainPosition += 0.05f;
+
+    // إعادة رسم الشاشة
+    glutPostRedisplay();
+
+    // تكرار كل 16ms ≈ 60 FPS
+    glutTimerFunc(16, update, 0);
+} 
